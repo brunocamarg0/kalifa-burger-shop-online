23e502b815938feba2c3bff7ebf26112d0ba6247
Bruno <brunocamargocontato@hotmail.com>
2025-07-12 20:23:57 -0300

    Altera rota de login do admin para /painel-da-dona para maior segurança

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/checkout" element={<Checkout />} />
-            <Route path="/admin/login" element={<AdminLogin />} />
+            <Route path="/painel-da-dona" element={<AdminLogin />} />
             <Route path="/admin" element={
               <ProtectedRoute>
                 <Admin />
